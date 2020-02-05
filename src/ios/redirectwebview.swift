b8814d1b4f07dbdadafb6616963433326ac9f168
vineeth S Thayyil <vineeth.thayyil@attinadsoftware.com>
2020-02-05 17:25:50 +0530

    initial

---
+++ src/ios/redirectwebview.swift
@@ -0,0 +1,24 @@
+//
+//  MyFirstPlugIn.swift
+//  myApp
+//
+//  Created by Vineeth on 2/5/20.
+//
+
+import Foundation
+
+@objc(YourPluginName) class native-wkwebview : CDVPlugin {
+  @objc(yourFunctionName:) // Declare your function name.
+  func callNativeFunction(command: CDVInvokedUrlCommand) { // write the function code.
+    /*
+     * Always assume that the plugin will fail.
+     * Even if in this example, it can't.
+     */
+    // Set the plugin result to fail.
+    var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");
+    // Set the plugin result to succeed.
+        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "The plugin succeeded");
+    // Send the function result back to Cordova.
+    self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+  }
+}
